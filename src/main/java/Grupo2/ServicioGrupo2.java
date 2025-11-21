48c63e63bf50fa67f4f86ef7e10158f6a27e7129
Dante Uchofen <dante.uchofen@usil.pe>
2025-11-20 23:16:47 -0500

    tarea completa

--- src/main/java/Grupo2/ServicioGrupo2.java
+++ src/main/java/Grupo2/ServicioGrupo2.java
@@ -4,10 +4,31 @@
  */
 package Grupo2;
 
+import Modelo.Producto;
+import java.util.List;
+
 /**
  *
  * @author user
  */
 public class ServicioGrupo2 {
+    public static boolean verificarLimite(double total){
+        return total<=5000;
+    }
     
+    public static boolean validarDescuento(double descuento){
+        return descuento>=0 && descuento<=50;
+    }
+    
+    public static double calcularIGV(double total){
+        return total*1.18;
+    }
+    
+    public static boolean validarCliente(String nombre) {
+        return nombre != null && !nombre.trim().isEmpty();
+    }
+    public static boolean verificarStock(List<Producto> productos){
+        return productos.stream()
+            .allMatch(p -> p.getCantidad() > 0);
+    }
 }
